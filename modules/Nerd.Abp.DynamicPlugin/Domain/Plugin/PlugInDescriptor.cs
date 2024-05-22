86ac27f3158cc7aca8fb9ea0701971204e4bc486
Ping <shui106@163.com>
2024-05-22 14:38:37 +0800

    misc: add 2 version fields

--- modules/Nerd.Abp.DynamicPlugin/Domain/Plugin/PlugInDescriptor.cs
+++ modules/Nerd.Abp.DynamicPlugin/Domain/Plugin/PlugInDescriptor.cs
@@ -1,5 +1,4 @@
-﻿using NUglify.JavaScript.Syntax;
-using System.Text.Json.Serialization;
+﻿using System.Text.Json.Serialization;
 using Volo.Abp.Modularity.PlugIns;
 
 namespace Nerd.Abp.DynamicPlugin.Domain.Plugin
@@ -9,6 +8,8 @@ namespace Nerd.Abp.DynamicPlugin.Domain.Plugin
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public bool IsEnabled { get; set; }
+        public string Version { get; set; } = string.Empty;
+        public string AbpVersion { get; set; } = string.Empty;
 
         [JsonIgnore]
         public IPlugInSource PlugInSource { get; set; }
