b5edd0759f27c8dbc0ac6965284561e6e7421ecf
Ping <shui106@163.com>
2024-06-07 17:12:20 +0800

    misc: rename theme

---
+++ themes/Nerd.Abp.LeptonxLiteTheme/NerdAbpNewFireThemeModule.cs
@@ -0,0 +1,13 @@
+﻿using Volo.Abp.AspNetCore.Mvc.UI.Theme.Shared;
+using Volo.Abp.Modularity;
+
+namespace Nerd.Abp.NewFireTheme
+{
+    [DependsOn(
+    typeof(AbpAspNetCoreMvcUiThemeSharedModule)
+    )]
+    public class NerdAbpNewFireThemeModule : AbpModule
+    {
+
+    }
+}
